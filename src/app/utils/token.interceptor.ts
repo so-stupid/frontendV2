1176fa43ad1ad93b77244c47e424a8bc71a6ee24
so-stupid <evankey2k17@yandex.ru>
2021-01-16 19:49:47 +0300

    add interceptor

---
+++ src/app/utils/token.interceptor.ts
@@ -0,0 +1,26 @@
+
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {KeycloakService} from 'keycloak-angular';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(private kcService: KeycloakService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authToken = this.kcService.getToken() || '';
+    request = request.clone({
+      setHeaders: {
+        'Authorization': 'Bearer ' + authToken
+      }
+    });
+    return next.handle(request);
+  }
+}
